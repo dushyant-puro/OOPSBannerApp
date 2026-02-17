deb41e47173fd3fab4d0e14b8d0e9392893b38f2
Dushyant Purohit <dushipurohit17@gmail.com>
2026-02-17 14:31:45 +0530

    first commit

---
+++ OOPSBannerApp.java
@@ -0,0 +1,14 @@
+public class OOPSBannerApp {
+   public OOPSBannerApp() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println(" *****   *****   ******   ***** ");
+      System.out.println("*     * *     *  *     * *     *");
+      System.out.println("*     * *     *  *     * *      ");
+      System.out.println("*     * *     *  ******   ***** ");
+      System.out.println("*     * *     *  *             *");
+      System.out.println("*     * *     *  *             *");
+      System.out.println(" *****   *****   *        ***** ");
+   }
+}
